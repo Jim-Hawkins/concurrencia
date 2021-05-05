8ac0b82a55529f331ae7fa3c0f2e08b91be1c156
CarlosChato <carlosyalgo20@gmail.com>
2021-05-05 16:39:57 +0200

    Avances en code principal

--- costCalculator.c
+++ costCalculator.c
@@ -7,7 +7,6 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include "queue.h"
-#include "queue.c"
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -25,7 +24,37 @@
  */
 int main (int argc, const char * argv[] ) {
 
-    int total = 0;
+
+    int total = 0; 
+    
+    FILE *file;
+    file = fopen(argv[1],"r"); //abrimos fichero
+
+    int NUM_PRODUC = atoi(argv[2]); //num de productores que habra
+
+    queue * q = queue_init(atoi(argv[3])); //declaramos la variable cola
+
+    int num_op; //num de operaciones a realizar
+
+    fscanf(file,"%d\n",&num_op);
+
+    printf("%d\n",num_op);
+
+    struct element * ops = (struct element *) malloc(sizeof(struct element)*num_op);
+
+    for(int i = 0;i<num_op;i++){
+
+	int id;
+	struct element elem;
+	fscanf(file,"%d %d %d\n",&id,&elem.type,&elem.time);
+	
+	printf("%d %d %d\n",id,elem.type,elem.time);	
+
+	}
+    
+    
+
+
     printf("Total: %i €.\n", total);
 
     return 0;
